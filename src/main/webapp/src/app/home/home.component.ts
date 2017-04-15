4bd646756b199cd76dac11ece3b49447b7f95ddb
nkharabaruk <nkharabaruk@gmail.com>
2017-04-15 17:46:12 +0300

    dropdown for days and persons

--- src/main/webapp/src/app/home/home.component.ts
+++ src/main/webapp/src/app/home/home.component.ts
@@ -10,19 +10,26 @@ import {PlaceService} from "../../shared/service/place.service";
 export class HomeComponent implements OnInit {
   places: Place[];
   dropdownTittle: string;
+  dayCount: number[];
+  personCount: number[];
+  day: string;
+  person: string;
 
   constructor(private placeService: PlaceService) { }
 
   ngOnInit() {
     this.dropdownTittle = "Місцевість";
+    this.dayCount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    this.personCount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    this.day = "Кількість днів";
+    this.person = "Кількість осіб";
     this.placeService.getPlaces().subscribe((places) => {
       this.places = places;
       console.log(places);
     });
   }
 
-  setButtonName(button, text){
-    console.log(button);
-    button.textContent = text;
+  setButtonName(place){
+    this.dropdownTittle = place.name;
   }
 }
